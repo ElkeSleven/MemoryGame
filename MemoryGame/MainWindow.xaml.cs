33b03781b89aca563fdaf672fb9d9645f4b801c7
Elke Sleven <myprogrammersmail@gmail.com>
2022-06-25 16:23:10 +0200

    Memory Game

--- MemoryGame/MainWindow.xaml.cs
+++ MemoryGame/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;  // DispatcherTimer
 
 namespace MemoryGame
 {
@@ -20,19 +21,142 @@ namespace MemoryGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+
+        Random r = new Random();
+        List<char> eazyMode;
+        Label clickFir;
+        Label clickSec;
+        private int increment;
+        private DispatcherTimer timer;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            DefaultValues();
+            SetImages(eazyMode);
         }
 
-        Random r = new Random();
-        List<char> eazyMode = new List<char> { '¨', 'à','l', 'n','w','+','h','d','²'};   // Webdings
+        private void DefaultValues()
+        {
+            eazyMode = new List<char> { '¨', 'à', 'l', 'n', 'w', '+', 'd', '²' };   // Webdings 'h'
+            increment = 0;
+            Label clickFir = null;
+            Label clickSec = null;
+        }
+        private void SetImages(List<char> list)
+        {
+            Label lbl;
+            int randomIndex;
+            List<char> clonedList = new List<char>(list); // clone the list 
+            list.AddRange(clonedList);            // Use .AddRange to append any Enumrable collection to the list.
+           
+            
+            for (int i = 0; i < grind.Children.Count; i++)
+            {
+                 if(grind.Children[i] is Label)   // check if selected child is a label  (expexted : TRUE)
+                 {
+                    lbl = (Label)grind.Children[i];     
+                    randomIndex = r.Next(0, list.Count);
+                    lbl.Content = list[randomIndex];
+                    list.RemoveAt(randomIndex);
+                    lbl.Foreground = Brushes.DeepSkyBlue;
 
-        Label clickFir;
-        Label clickSec;
+                 }
+                  
 
-       
+            }  
 
 
+        }
+
+        private void StartTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0,0,0,1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            increment++;
+            if(increment == 1)
+            {  
+                timer.Stop();
+                increment = 0;
+                clickFir.Foreground = Brushes.DeepSkyBlue;
+                clickSec.Foreground = Brushes.DeepSkyBlue;
+                clickFir = null;
+                clickSec = null;
+            }
+        }
+
+        private void ClickImage(object sender, MouseButtonEventArgs e)
+        {
+            if(clickFir != null && clickSec != null)
+            {
+                return;
+            }
+            else if (sender is Label)
+            {
+                Label clicked = sender as Label;
+                if (clicked.Foreground == Brushes.DeepSkyBlue && clickFir == null)
+                {
+                    clickFir = clicked;
+                    clicked.Foreground = Brushes.Black;
+                }
+                else if (clicked.Foreground == Brushes.DeepSkyBlue && clickSec == null && clickFir != null)
+                {
+                    clickSec = clicked;
+                    clicked.Foreground = Brushes.Black;
+                }
+                else if (clicked.Foreground != Brushes.DeepSkyBlue)
+                {
+                    return;
+                }
+
+                if (clickFir != null && clickSec != null)
+                {
+                    CheckPair(clickFir, clickSec);
+                }
+                return;
+            }
+            
+
+
+        }
+        private void CheckPair(Label a , Label b)
+        {
+            if(a.Content.ToString() == b.Content.ToString())
+            {
+                clickFir.Foreground = Brushes.Black;
+                clickSec.Foreground = Brushes.Black;
+                clickFir = null;
+                clickSec = null;
+                CheckEndGame();
+                return;
+            }
+            else if (a.Content != b.Content)
+            {
+                StartTimer();
+                return;
+            }
+
+        }
+
+        private void CheckEndGame()
+        {
+            Label lbl;
+            for (int i = 0; i < grind.Children.Count; i++)
+            {
+                lbl = grind.Children[i] as Label; 
+                if(lbl.Foreground == Brushes.DeepSkyBlue) { return; }
+            }
+
+            MessageBox.Show("you wonn XD ");
+            Close();
+        }
     }
 }
